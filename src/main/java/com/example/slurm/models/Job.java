965ebb86d43203f1a2aac8d01d747390a2bb054a
aluera <64332771+aluera@users.noreply.github.com>
2020-11-10 12:59:21 +0400

    -

--- src/main/java/com/example/slurm/models/Job.java
+++ src/main/java/com/example/slurm/models/Job.java
@@ -1,6 +1,8 @@
 package com.example.slurm.models;
 
 import javax.persistence.*;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @Entity
 @Table(name = "ssucluster_job_table")
@@ -10,24 +12,91 @@ public class Job {
     @Column(name = "job_db_inx")
     private Integer id;
 
-    @Column(name = "mod_time")
-    private Integer modTime;
+    @Column(name = "account")
+    private String account;
 
-    public Integer getId() {
-        return id;
+    @Column(name = "cpus_req")
+    private Integer cpus_req;
+
+    @Column(name = "exit_code")
+    private Integer exitCode;
+
+    @Column(name = "time_start")
+    private Long timeStart;
+
+    @Column(name = "time_end")
+    private Long timeEnd;
+
+    //Секнуды во времени выполнение задачи
+    @Transient
+    public long time() {
+        Date start = new Date(timeStart);
+        Date end = new Date(timeEnd);
+
+        long diffInMillies = Math.abs(end.getTime() - start.getTime());
+        long diff = TimeUnit.SECONDS.convert(diffInMillies, TimeUnit.SECONDS);
+
+        return diff;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    @Column(name = "work_dir")
+    private String workDir;
+
+
+    @Column(name = "partition")
+    private String partition;
+
+    @Column(name = "nodelist")
+    private String nodeList;
+
+    @Column(name = "id_job")
+    private Integer id_Job;
+
+    @Column(name = "job_name")
+    private String job_Name;
+
+    public String getJob_Name() {
+        return job_Name;
     }
 
-    public Integer getModTime() {
-        return modTime;
+    public Integer getId_Job() {
+        return id_Job;
     }
 
-    //public void setModTime(Integer modTime) {
-    //    this.modTime = modTime;
-    //}
+    public String getAccount() {
+        return account;
+    }
 
+    public Integer getCpus_req() {
+        return cpus_req;
+    }
 
+    //Queue Очередь
+    public String getPartition() {
+        return partition;
+    }
+
+    public String getNodeList() {
+        return nodeList;
+    }
+
+    public Integer getExitCode() {
+        return exitCode;
+    }
+
+    public Long getTimeStart() {
+        return timeStart;
+    }
+
+    public Long getTimeEnd() {
+        return timeEnd;
+    }
+
+    public long getTime() {
+        return time();
+    }
+
+    public String getWorkDir() {
+        return workDir;
+    }
 }
