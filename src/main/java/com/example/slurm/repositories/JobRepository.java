ebad7baaf58c690015422b8258d5ffab49297ef0
nnorienn <greentech676@gmail.com>
2020-11-17 00:29:18 +0400

    nothing

--- src/main/java/com/example/slurm/repositories/JobRepository.java
+++ src/main/java/com/example/slurm/repositories/JobRepository.java
@@ -1,9 +1,8 @@
 package com.example.slurm.repositories;
 
 import com.example.slurm.models.Job;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface JobRepository extends JpaRepository<Job, Integer> {
+public interface JobRepository extends JpaRepository<Job, Integer>, JpaSpecificationExecutor<Job> {
 }
