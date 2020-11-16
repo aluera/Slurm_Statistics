dbdec68a86e93cda0a54bc165d0d42f6a77f8feb
nnorienn <greentech676@gmail.com>
2020-11-16 19:35:10 +0400

    add pagination

--- src/main/java/com/example/slurm/repositories/JobRepository.java
+++ src/main/java/com/example/slurm/repositories/JobRepository.java
@@ -1,7 +1,9 @@
 package com.example.slurm.repositories;
 
 import com.example.slurm.models.Job;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface JobRepository extends CrudRepository<Job, Integer> {
+public interface JobRepository extends JpaRepository<Job, Integer> {
 }
